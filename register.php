5f6c153d874d662d95356244bbc11986bfc9853f
David Larabal <davidkivuyo123@gmail.com>
2026-05-14 17:05:30 +0300

    removed the GPL LICENCE, added register page

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+?>
+<!--FILE UNUSED UNTIL FOR FUTURE DEVELOPMENT-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create an account</title>
+</head>
+<body>
+    
+</body>
+</html>
